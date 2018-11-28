9ca5edee59baf21b72a9c93adfc544c162d43fc4
Maroua Mesbahi <maroua.nebiya@gmail.com>
2018-11-28 21:39:24 +0100

    Kernel: Keyboard working

--- k/init_idt.c
+++ k/init_idt.c
@@ -3,72 +3,11 @@
 
 #include "multiboot.h"
 #include "io.h"
-
+#include "keyboard.h"
 #include "idt.h"
 
-#define KEYBOARD_STATUS_PORT 0x64
-#define KEYBOARD_DATA_PORT 0x60
-
-extern void keyboard_handler ();
-// These extern directives let us access the addresses of our ASM ISR handlers.
-extern void isr0 ();
-extern void isr1 ();
-extern void isr2 ();
-extern void isr3 ();
-extern void isr4 ();
-extern void isr5 ();
-extern void isr6 ();
-extern void isr7 ();
-extern void isr8 ();
-extern void isr9 ();
-extern void isr10();
-extern void isr11();
-extern void isr12();
-extern void isr13();
-extern void isr14();
-extern void isr15();
-extern void isr16();
-extern void isr17();
-extern void isr18();
-extern void isr19();
-extern void isr20();
-extern void isr21();
-extern void isr22();
-extern void isr23();
-extern void isr24();
-extern void isr25();
-extern void isr26();
-extern void isr27();
-extern void isr28();
-extern void isr29();
-extern void isr30();
-extern void isr31();
-
-
-// IRQ Handlers
-extern void irq0();
-extern void irq1();
-extern void irq2();
-extern void irq3();
-extern void irq4();
-extern void irq5();
-extern void irq6();
-extern void irq7();
-extern void irq8();
-extern void irq9();
-extern void irq10();
-extern void irq11();
-extern void irq12();
-extern void irq13();
-extern void irq14();
-extern void irq15();
-
-
-
-struct idtdesc idt[IDTSIZE];
 struct idt_r idtr;
-
-
+struct idtdesc idt[IDTSIZE];
 
 void idt_set_gate(int num, u32 base, u16 sel, u8 flags)
 {
@@ -76,16 +15,15 @@ void idt_set_gate(int num, u32 base, u16 sel, u8 flags)
   idt[num].base_hi = (base >> 16) & 0xFFFF;
   idt[num].sel     = sel;
   idt[num].always0 = 0;
-// We must uncomment the OR below when we get to using user-mode.
-// It sets the interrupt gate's privilege level to 3.
-  idt[num].flags   = flags /* | 0x60 */;
+  idt[num].flags   = flags;
 }
 
 void init_idt(void)
 {
-    idtr.base = (u32)idt; /* idt base address */
-    idtr.limite = sizeof(idt) - 1; /* idt size - 1 */
+    idtr.base = (u32)idt;
+    idtr.limite = sizeof(idt) - 1;
 
+    // isr entries
     idt_set_gate( 0, (u32)isr0 , 0x08, 0x8E);
     idt_set_gate( 1, (u32)isr1 , 0x08, 0x8E);
     idt_set_gate( 2, (u32)isr2, 0x08, 0x8E);
@@ -138,25 +76,27 @@ void init_idt(void)
     idt_set_gate(46, (u32)irq14, 0x08, 0x8E);
     idt_set_gate(47, (u32)irq15, 0x08, 0x8E);
 
+    //load
     __asm__ volatile("lidt %0\n"
       : /* no output */
       : "m" (idtr)
       : "memory");
 }
 
-void keyboard_handler_main(void) {
-	unsigned char status;
+int getkey(void) {
+  unsigned char status;
 	char keycode;
   outb(0x20, 0x20);
-	status = inb(KEYBOARD_STATUS_PORT);
+  status = inb(KEYBOARD_STATUS_PORT);
 	if (status & 0x01) {
 		keycode = inb(KEYBOARD_DATA_PORT);
-    printf("%d**\n", keycode);
 		if(keycode < 0)
-			return;
-		//vidptr[current_loc++] = keyboard_map[keycode];
-		//vidptr[current_loc++] = 0x07;
+			return -1;
+		char c = keyboard_map[keycode];
+    printf("%c", c);
+    return c;
 	}
+  return -1;
 }
 
 void generate_c_handler(registers_t regs)
@@ -164,14 +104,5 @@ void generate_c_handler(registers_t regs)
     if(regs.int_no < 32)
     {
         printf("Exception. Code: %d", regs.int_no);
-    }
-}
-
-void irq_handler(registers_t regs)
-{
-    //If int_no >= 40, we must reset the slave as well as the master
-    if(regs.int_no >= 40)
-    {
-        printf("helloooooooooo");
     }
 }
--- k/pic.c
+++ k/pic.c
@@ -4,31 +4,62 @@
 #include "io.h"
 #include "idt.h"
 
-void init_pic(void)
+/*void init_pic(void)
 {
-    /* Initialization of ICW1 */
+
     outb(0x20, 0x11);
     outb(0xA0, 0x11);
 
-    /* Initialization of ICW2 */
+
     outb(0x21, 0x20);
     outb(0xA1, 0x28);
 
-    /* Initialization of ICW3 */
+
     outb(0x21, 0x04);
     outb(0xA1, 0x02);
 
-    /* Initialization of ICW4 */
+
     outb(0x21, 0x01);
     outb(0xA1, 0x01);
 
-    /* Keyboard */
+
     outb(0x21, 0xFD);
     outb(0xA1, 0xFF);
 
-    /* mask interrupts */
+
     outb(0x20, 0x20);
     outb(0xA0, 0x20);
 
     __asm__("sti\n");
+}*/
+
+void init_pic(void)
+{
+  /* ICW1 - begin initialization */
+	outb(0x20 , 0x11);
+	outb(0xA0 , 0x11);
+
+	/* ICW2 - remap offset address of IDT */
+	/*
+	* In x86 protected mode, we have to remap the PICs beyond 0x20 because
+	* Intel have designated the first 32 interrupts as "reserved" for cpu exceptions
+	*/
+	outb(0x21 , 0x20);
+	outb(0xA1 , 0x28);
+
+	/* ICW3 - setup cascading */
+	outb(0x21 , 0x00);
+	outb(0xA1 , 0x00);
+
+	/* ICW4 - environment info */
+	outb(0x21 , 0x01);
+	outb(0xA1 , 0x01);
+	/* Initialization finished */
+
+	/* mask interrupts */
+	outb(0x21 , 0xff);
+	outb(0xA1 , 0xff);
+  outb(0x21 , 0xFD);
+  __asm__("sti\n");
+
 }
